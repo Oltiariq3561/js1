f834f47ba1e42e76924dcea5345653d9657d8e3c
Oltiariq3561 <oltiariq356@gmail.com>
2024-05-16 13:02:10 -0700

    js

---
+++ index.js
@@ -0,0 +1,186 @@
+// 1-misol
+
+// let L = + prompt("uzunlik");
+
+// let m = L / 100;
+
+// m = parseInt(m);
+
+// document.write (m);
+
+
+// 2-mosol
+
+// let M = + prompt("kilogramni yozing");
+
+// let l = M / 1000;
+
+// l = parseInt(l);
+
+// document.write (l);
+
+
+// 3- misol
+
+// let a = + prompt("baytlarda yozing");
+
+// let b = a / 1024 ;
+
+// b = parseInt(b);
+
+// document.write (b);
+
+
+// 4-misol
+
+// let a = + prompt ("a kesma");
+
+// let b = + prompt ("b kesma");
+
+// let c = a /b ;
+
+// c = parseInt(c);
+
+// document.write (c);
+
+
+// 6-misol
+
+// let a = + prompt ("2honali son yozing");
+
+// let b = a / 10;
+
+// b = parseInt(b);
+
+// let c = a % 10;
+
+// c = parseInt(c)
+
+// document.write(b);
+
+// document.write(c);
+
+
+// 7-misol
+
+// let a = + prompt ("2 xonali son");
+
+// let b = a / 10;
+
+// let c = a % 10;
+
+// b = parseInt(b);
+
+// document.write (b+c);
+
+
+// 8-misol
+
+// let a = + prompt ("2 xonali son");
+
+// let b = a / 10;
+
+// let c = a % 10;
+
+// b = parseInt(b);
+
+// document.write (c);
+
+// document.write (b);
+
+
+// 9-misol
+
+// let a = + prompt ("3honali son yozing");
+
+// let b = a / 100;
+
+// b = parseInt(b);
+
+// document.write(b);
+
+
+// 10-misol
+
+// let a = + prompt ("3 honali son yozing");
+
+// let b = a % 100 ;
+
+// b = parseInt(b); 
+
+// let c = b % 10 ;
+
+// c = parseInt(c)
+
+// let d = b / 10 ;
+
+// d = parseInt(d);
+
+// document.write(c);
+
+// document.write(d);
+
+
+// 11-misol
+
+// let a = + prompt ("3 honali son yozing")
+
+// let b = a % 10;
+
+// let c = a % 100;
+
+// let d = c / 10;
+
+// d = parseInt(d);
+
+// let e = a / 100;
+
+// e = parseInt(e);
+
+// document.write(b+d+e);
+
+
+// 12-misol
+
+// let a = + prompt ("3 honali son yozing")
+
+// let b = a % 10;
+
+// let c = a % 100;
+
+// let d = c / 10;
+
+// d = parseInt(d);
+
+// let e = a / 100;
+
+// e = parseInt(e);
+
+// document.write(b);
+
+// document.write(d);
+
+// document.write(e);
+
+
+// 13-misol
+
+// let a = + prompt ("3 honali son yozing")
+
+// let b = a % 10;
+
+// let c = a % 100;
+
+// let d = c / 10;
+
+// d = parseInt(d);
+
+// let e = a / 100;
+
+// e = parseInt(e);
+
+// document.write(d);
+
+// document.write(b);
+
+// document.write(e);
